6d5ba24ca5e6a66a43a20a8dc7660f6a8d0037a4
ibrahimkhalilullahshafi <shafi.warx@gmail.com>
2022-11-10 15:55:27 +0600

    2nd section added in home

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Button } from 'react-bootstrap';
 import { Link, useLoaderData } from 'react-router-dom';
 import Services from '../Shared-components/Services/Services';
-import photo from '../../poster.jpg'
+import photo from '../../poster.jpg';
+import pic1 from './Scoot.png'
+import pic2 from './Store.png'
+import pic3 from './phone.png'
+
 
 const Home = () => {
     const services = useLoaderData();
@@ -20,7 +24,25 @@ const Home = () => {
                 )}
             </div>
             <Link to="/services" className='d-flex justify-content-center'><Button className='w-10px' variant="primary">See all</Button></Link>
-        </div>
+            <div className='d-flex flex-row'>
+                <div className='mx-3 my-5 text-center justify-content-evenly'>
+                    <img className='img-fluid' src={pic1} alt="" />
+                    <h2>Become a hero</h2>
+                    <p>As a delivery driver, you'll make money-working anytime.</p>
+                </div>
+                <div className='mx-3 my-5 text-center justify-content-evenly'>
+                    <img className='img-fluid' src={pic2} alt="" />
+                    <h2>Become a partner</h2>
+                    <p>Grow your business and reach new customers with us.</p>
+                </div>
+                <div className='mx-3 my-5 text-center justify-content-evenly'>
+                    <img className='img-fluid' src={pic3} alt="" />
+                    <h2>Try our app</h2>
+                    <p>Experience your choices for food,all in one app.</p>
+                </div>
+
+            </div>
+        </div >
     );
 };
 
